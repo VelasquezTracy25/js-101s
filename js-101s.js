18fb285dd9233be3ffdb613470429af1948c00b2
Tracy Velasquez <velasqueztracy25@gmail.com>
2020-09-28 23:27:54 -0500

    completed first 5

--- js-101s.js
+++ js-101s.js
@@ -39,6 +39,7 @@ assert(doingJSRightNow, true, "Exercise 0");
 //  Exercise 1
 // On the line below, create a variable named onMarsRightNow and assign it the boolean value of false
 
+let onMarsRightNow = false;
 
 assert(onMarsRightNow, false, "Exercise 1");
 addToDone("Exercise 1 is correct.");
@@ -48,6 +49,8 @@ addToDone("Exercise 1 is correct.");
 // Create a variable named fruits and assign it an array of strings containing the following fruits.
 // mango, banana, guava, kiwi, and strawberry.
 
+let fruits = ["mango", "banana", "guava", "kiwi", "strawberry"];
+
 assert(fruits,["mango", "banana", "guava", "kiwi", "strawberry"], "Exercise 2");
 addToDone("Exercise 2 is correct.");
 
@@ -55,12 +58,16 @@ addToDone("Exercise 2 is correct.");
 //  Create a variable named vegetables and assign it an array of fruits containing the following vegetable names as strings:
 //  eggplant, broccoli, carrot, cauliflower, and zucchini
 
+let vegetables = ["eggplant", "broccoli", "carrot", "cauliflower", "zucchini"];
+
 assert(vegetables, ["eggplant", "broccoli", "carrot", "cauliflower", "zucchini"], "Exercise 3");
 addToDone("Exercise 3 is correct.");
 
 
 // Exercise 4
 // Create a variable named numbers and assign it an array of numbers, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 assert(numbers, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], "Exercise 4");
 addToDone("Exercise 4 is correct.");
@@ -70,6 +77,8 @@ addToDone("Exercise 4 is correct.");
 // Add the string "tomato" to the end of the fruits array.
 // *Hint* Recommend finding and using a built-in JS operation to add to an array rather than recreating the array.
 
+fruits.push("tomato");
+console.log(fruits);
 
 assert(fruits, ["mango", "banana", "guava", "kiwi", "strawberry", "tomato"], "Exercise 5");
 addToDone("Exercise 5 is correct");
